5e2a3b1a84bc57a33b29b95a76da938ccb953e7f
Fares <122798605+FaresEisa@users.noreply.github.com>
2023-03-09 19:17:53 +0000

    Update Block Breaker.rkt
    Added the blocks, changed the speed of which is takes you to move the bar left to right. (From 5 to 20) 
    Need to add a functions to the blocks, so that 1) once the block is hit by the ball it disappears  2) once the block is hit the ball bounces back.

--- Block Breaker.rkt
+++ Block Breaker.rkt
@@ -14,6 +14,42 @@
 (define BALL (circle 6 "solid" "green"))
 (define BAR (rectangle 50 5 "solid" "green"))
 
+;First Row Block1 = a-block1; also block1 is in the middle.
+(define block1 (rectangle 30 7 "solid" "red"))
+(define block2 (rectangle 30 7 "solid" "Brown"))
+(define block3 (rectangle 30 7 "solid" "Green"))
+(define block4 (rectangle 30 7 "solid" "Orange"))
+(define block5 (rectangle 30 7 "solid" "Pink"))
+(define block6 (rectangle 30 7 "solid" "Purple"))
+(define block7 (rectangle 30 7 "solid" "Turquoise"))
+(define block8 (rectangle 30 7 "solid" "Yellow"))
+(define block9 (rectangle 30 7 "solid" "Goldenrod"))
+(define block10(rectangle 30 7 "solid" "Dark Pink"))
+
+;Second Row
+(define block11 (rectangle 30 7 "solid" "Light Brown"))
+(define block12 (rectangle 30 7 "solid" "Medium Brown"))
+(define block13 (rectangle 30 7 "solid" "Dark Brown"))
+(define block14 (rectangle 30 7 "solid" "Medium Cyan"))
+(define block15 (rectangle 30 7 "solid" "Light Goldenrod"))
+(define block16 (rectangle 30 7 "solid" "Medium Gray"))
+(define block17 (rectangle 30 7 "solid" "Medium Green"))
+(define block18 (rectangle 30 7 "solid" "Light Orange"))
+(define block19 (rectangle 30 7 "solid" "Medium Orange"))
+(define block20 (rectangle 30 7 "solid" "Medium Pink"))
+
+;Third Row
+(define block21 (rectangle 30 7 "solid" "red"))
+(define block22 (rectangle 30 7 "solid" "Brown"))
+(define block23 (rectangle 30 7 "solid" "Green"))
+(define block24 (rectangle 30 7 "solid" "Orange"))
+(define block25 (rectangle 30 7 "solid" "Pink"))
+(define block26 (rectangle 30 7 "solid" "Purple"))
+(define block27 (rectangle 30 7 "solid" "Turquoise"))
+(define block28 (rectangle 30 7 "solid" "Yellow"))
+(define block29 (rectangle 30 7 "solid" "Goldenrod"))
+(define block30 (rectangle 30 7 "solid" "Dark Pink"))
+
 (define (UPDATE_POSITION ball)
   (set-state-x! a-ball (+ (state-x a-ball) (state-vx a-ball)))
   (set-state-y! a-ball (+ (state-y a-ball) (state-vy a-ball)))
@@ -73,29 +109,119 @@
 
 (define (MOVE w a-key)
   (cond
-    [(key=? a-key "left") (set-state-x! bar (- (state-x bar) 5))]
-    [(key=? a-key "right") (set-state-x! bar (+ (state-x bar) 5))]
+    [(key=? a-key "left") (set-state-x! bar (- (state-x bar) 20))]
+    [(key=? a-key "right") (set-state-x! bar (+ (state-x bar) 20))]
     [#t w] ; order-free checking
     )
   )
 
 (define (OBJECTS state)
-  (place-image BALL
-               (state-x a-ball)
-               (state-y a-ball)
-               (place-image VERTICAL_RECTANGLE
-                            5
-                            255
-                            (place-image VERTICAL_RECTANGLE
-                                         495
-                                         255
-                                         (place-image HORIZONTAL_RECTANGLE
-                                                      250
-                                                      5
-                                                      (place-image BAR
-                                                      (state-x bar)
-                                                      (state-y bar)
-                                                      SCENE))))))
+(place-image BALL
+        (state-x a-ball)
+        (state-y a-ball)
+(place-image VERTICAL_RECTANGLE
+        5
+        255
+(place-image VERTICAL_RECTANGLE
+        495
+        255
+(place-image block1
+       (state-x a-block1)
+       (state-y a-block1)
+(place-image block2
+       (state-x a-block2)
+       (state-y a-block2)
+(place-image block3
+       215
+       150
+(place-image block4
+       180
+       150
+(place-image block5
+       145
+       150
+(place-image block6
+       110
+       150
+(place-image block7
+       75
+       150
+(place-image block8
+       320
+       150
+(place-image block9
+       355
+       150
+(place-image block10
+       390
+       150
+(place-image block11
+       250
+       137
+(place-image block12
+       285
+       137
+(place-image block13
+       215
+       137
+(place-image block14
+       180
+       137
+(place-image block15
+       145
+       137
+(place-image block16
+       110
+       137
+(place-image block17
+       75
+       137
+(place-image block18
+       320
+       137
+(place-image block19
+       355
+       137
+(place-image block20
+       390
+       137
+(place-image block21
+       250
+       125
+(place-image block22
+       285
+       125
+(place-image block23
+       215
+       125
+(place-image block24
+       180
+       125
+(place-image block25
+       145
+       125
+(place-image block26
+       110
+       125
+(place-image block27
+       75
+       125
+(place-image block28
+       320
+       125
+(place-image block29
+       355
+       125
+(place-image block30
+       390
+       125
+(place-image HORIZONTAL_RECTANGLE
+       250
+       5
+(place-image BAR
+(state-x bar)
+(state-y bar)
+SCENE))))))))))))))))))))))))))))))))))))
 
 (big-bang a-ball
   (on-tick UPDATE_POSITION 1/120)
